f42d099be200e6170668ef4ccddf8b3bf2902567
Joe <1264204425@qq.com>
2024-02-18 17:33:48 +0800

    feat: remove disable_lora()

--- predict.py
+++ predict.py
@@ -113,7 +113,6 @@ class Predictor(BasePredictor):
         if self.loaded == merged_fn:
             print("The requested LoRAs are loaded.")
         else:
-            self.pipe.disable_lora()
             self.pipe.unload_lora_weights()
             # 保存下载后的权重文件名
             lora_weight_files = []
@@ -345,7 +344,6 @@ class Predictor(BasePredictor):
             )
 
         # 清除所有lora模型
-        self.pipe.disable_lora()
         self.pipe.unload_lora_weights()
         self.loaded = None
         self.lora_manager = None
