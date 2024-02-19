70b9b7d57e8872e2aabecb087f6ad5d1b8e83f64
Joe <1264204425@qq.com>
2024-02-19 12:49:07 +0800

    feat: add dreamshaper_8_with_lora

--- deploy_others.sh
+++ deploy_others.sh
@@ -1,8 +1,10 @@
-export MODEL_ID="jzli/majicMIX-realistic-7" # change this
+#export MODEL_ID="jzli/majicMIX-realistic-7" # change this
+export MODEL_ID="Lykon/dreamshaper-8" # change this
 export SAFETY_MODEL_ID="CompVis/stable-diffusion-safety-checker"
 export IS_FP16=1
 export USERNAME="zhouhaojiang" # change this
-export REPLICATE_MODEL_ID="majicmix_with_lora" # change this
+#export REPLICATE_MODEL_ID="majicmix_with_lora" # change this
+export REPLICATE_MODEL_ID="dreamshaper_8_with_lora" # change this
 
 echo "MODEL_ID=$MODEL_ID" > .env
 echo "SAFETY_MODEL_ID=$SAFETY_MODEL_ID" >> .env
