7f90c4399c5e758015f6b8048a212334d52c7a35
Joe <1264204425@qq.com>
2024-02-19 13:56:34 +0800

    feat: add dark-sushi-mix_with_lora

--- deploy_others.sh
+++ deploy_others.sh
@@ -1,10 +1,11 @@
 #export MODEL_ID="jzli/majicMIX-realistic-7" # change this
-export MODEL_ID="Lykon/dreamshaper-8" # change this
+#export MODEL_ID="stablediffusionapi/dreamshaper-v8" # change this
+export MODEL_ID="stablediffusionapi/dark-sushi-mix"
 export SAFETY_MODEL_ID="CompVis/stable-diffusion-safety-checker"
 export IS_FP16=1
 export USERNAME="zhouhaojiang" # change this
 #export REPLICATE_MODEL_ID="majicmix_with_lora" # change this
-export REPLICATE_MODEL_ID="dreamshaper_8_with_lora" # change this
+export REPLICATE_MODEL_ID="dark_sushi_mix_with_lora" # change this
 
 echo "MODEL_ID=$MODEL_ID" > .env
 echo "SAFETY_MODEL_ID=$SAFETY_MODEL_ID" >> .env
